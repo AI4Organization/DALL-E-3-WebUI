2ada9a5590d49cd6469a797a99bd7a536f51eb89
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-11-07 19:36:56 +0700

    Use model dall-e-3

--- pages/api/images.js
+++ pages/api/images.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
     prompt: req.query.p,
     n: parseInt(req.query.n),
     size: "1024x1024",
+    model: 'dall-e-3'
   });
   console.log(response.data);
   res.status(200).json({ result: response.data });
