0b9dbfc1435cd8982ce898abc12d0fefa9ca0fe5
Trung Kenny Dinh <xamarindevelopervietnam@outlook.com>
2023-11-12 22:50:36 +0700

    Update pages/index.js
    Co-authored-by: codiumai-pr-agent[bot] <138128286+codiumai-pr-agent[bot]@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 export default function Home() {
   const [prompt, setPrompt] = useState("");
-  const [numImages, setNumImages] = useState(1);
+  const [numberOfImages, setNumberOfImages] = useState(1);
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
